d5bf877d0dbc64158f816ddb49a01d9959b34c96
Rahul Shah <shahrahul795@gmail.com>
2022-02-22 15:59:24 +0530

    [LAZY LOADING] - WIP code

---
+++ lib/widgets/utils/header_builder.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class TableHeader extends StatelessWidget {
+  const TableHeader({Key? key, required this.dataList}) : super(key: key);
+  final List dataList;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: Colors.grey[300]),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 6),
+              child: Text(
+                dataList[0],
+                style: Theme.of(context).primaryTextTheme.headline4,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 6),
+              child: Text(
+                dataList[1],
+                style: Theme.of(context).primaryTextTheme.headline4,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 6),
+              child: Text(
+                dataList[2],
+                style: Theme.of(context).primaryTextTheme.headline4,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 6),
+              child: Text(
+                dataList[3],
+                style: Theme.of(context).primaryTextTheme.headline4,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
