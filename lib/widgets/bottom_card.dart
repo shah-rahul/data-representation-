64845c38eae3da50ac38e6939974e4ae30ddd4cf
Rahul Shah <shahrahul795@gmail.com>
2022-03-04 00:09:13 +0530

    [UI UPDATE] - fixed bottom cards

---
+++ lib/widgets/bottom_card.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:nudron/widgets/sample_chart.dart';
+
+import '../screens/map_page.dart';
+import 'level1/device_history.dart';
+
+class BottomCard extends StatelessWidget {
+  const BottomCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 5),
+      child: DefaultTabController(
+        length: 3,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              height: 10,
+              width: MediaQuery.of(context).size.width * 0.9,
+              decoration: BoxDecoration(
+                  color: Colors.yellow[700],
+                  borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(5),
+                      topRight: Radius.circular(5))),
+            ),
+            Container(
+              height: 40,
+              width: MediaQuery.of(context).size.width * 0.9,
+              decoration: const BoxDecoration(
+                  color: Colors.black87,
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(5),
+                      bottomRight: Radius.circular(5))),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TabBar(
+                    unselectedLabelColor: Colors.white,
+                    indicatorPadding: const EdgeInsets.only(left: 5, right: 5),
+                    indicator: BoxDecoration(
+                        color: Colors.grey[600],
+                        borderRadius: BorderRadius.circular(8)),
+                    tabs: const [
+                      Tab(
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text("Device history"),
+                        ),
+                      ),
+                      Tab(
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text("Chart"),
+                        ),
+                      ),
+                      Tab(
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text("Map"),
+                        ),
+                      ),
+                    ]),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.4,
+              child: TabBarView(
+                  physics: const NeverScrollableScrollPhysics(),
+                  children: [
+                    FittedBox(
+                        fit: BoxFit.scaleDown,
+                        child: DeviceHistory()),
+                    const Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: CustombarChart(),
+                    ),
+                    MapPage(),
+                  ]),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
