07f9165ce50390ac06a8c70585f8c73334fdb297
Rahul Shah <shahrahul795@gmail.com>
2022-03-24 00:07:52 +0530

    [BACKEND] - data feeding to graphs

---
+++ lib/providers/chartDataProvider.dart
@@ -0,0 +1,436 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:nudron/models/chart_data.dart';
+
+class ChartDataProvider extends ChangeNotifier {
+  List<ChartData> alerts2020 = [];
+  List<ChartData> alerts2021 = [];
+  List<ChartData> alerts2022 = [];
+  List<ChartData> usage2020 = [];
+  List<ChartData> usage2021 = [];
+  List<ChartData> usage2022 = [];
+
+  void initCall() {
+    readYear2020();
+    readYear2021();
+    readYear2022();
+  }
+
+  Future<void> readYear2020() async {
+    final String response =
+        await rootBundle.loadString('assets/yearlyData.json');
+    final data = await json.decode(response) as List;
+    int jan = 0;
+    int feb = 0;
+    int march = 0;
+    int april = 0;
+    int may = 0;
+    int june = 0;
+    int july = 0;
+    int august = 0;
+    int september = 0;
+    int october = 0;
+    int november = 0;
+    int december = 0;
+    int jan1 = 0;
+    int feb1 = 0;
+    int march1 = 0;
+    int april1 = 0;
+    int may1 = 0;
+    int june1 = 0;
+    int july1 = 0;
+    int august1 = 0;
+    int september1 = 0;
+    int october1 = 0;
+    int november1 = 0;
+    int december1 = 0;
+
+    // ignore: avoid_function_literals_in_foreach_calls
+    data.forEach((element) {
+      var ts = element[0] * 86400000;
+      var date = DateTime.fromMillisecondsSinceEpoch(ts);
+      if (date.year == 2020) {
+        switch (date.month) {
+          case 1:
+            {
+              jan = jan + element[1] as int;
+              jan1 = jan1 + element[2] as int;
+            }
+
+            break;
+          case 2:
+            {
+              feb = feb + element[1] as int;
+              feb1 = feb1 + element[2] as int;
+            }
+
+            break;
+          case 3:
+            {
+              march = march + element[1] as int;
+              march1 = march1 + element[2] as int;
+            }
+            break;
+          case 4:
+            {
+              april = april + element[1] as int;
+              april1 = april1 + element[2] as int;
+            }
+            break;
+          case 5:
+            {
+              may = may + element[1] as int;
+              may1 = may1 + element[2] as int;
+            }
+            break;
+          case 6:
+            {
+              june = june + element[1] as int;
+              june = june1 + element[2] as int;
+            }
+            break;
+          case 7:
+            {
+              july = july + element[1] as int;
+              july = july1 + element[2] as int;
+            }
+            break;
+          case 8:
+            {
+              august = august + element[1] as int;
+              august1 = august1 + element[2] as int;
+            }
+            break;
+          case 9:
+            {
+              september = september + element[1] as int;
+              september1 = september1 + element[2] as int;
+            }
+            break;
+          case 10:
+            {
+              october = october + element[1] as int;
+              october1 = october1 + element[2] as int;
+            }
+            break;
+          case 11:
+            {
+              november = november + element[1] as int;
+              november1 = november1 + element[2] as int;
+            }
+            break;
+          case 12:
+            {
+              december = december + element[1] as int;
+              december1 = december1 + element[2] as int;
+            }
+            break;
+          default:
+        }
+      }
+    });
+    usage2020.add(ChartData(month: "janaury", data: jan));
+    usage2020.add(ChartData(month: "February", data: feb));
+    usage2020.add(ChartData(month: "March", data: march));
+    usage2020.add(ChartData(month: "April", data: april));
+    usage2020.add(ChartData(month: "May", data: may));
+    usage2020.add(ChartData(month: "June", data: june));
+    usage2020.add(ChartData(month: "July", data: july));
+    usage2020.add(ChartData(month: "August", data: august));
+    usage2020.add(ChartData(month: "Sptember", data: september));
+    usage2020.add(ChartData(month: "October", data: october));
+    usage2020.add(ChartData(month: "November", data: november));
+    usage2020.add(ChartData(month: "December", data: december));
+    alerts2020.add(ChartData(month: "janaury", data: jan1));
+    alerts2020.add(ChartData(month: "February", data: feb1));
+    alerts2020.add(ChartData(month: "March", data: march1));
+    alerts2020.add(ChartData(month: "April", data: april1));
+    alerts2020.add(ChartData(month: "May", data: may1));
+    alerts2020.add(ChartData(month: "June", data: june1));
+    alerts2020.add(ChartData(month: "July", data: july1));
+    alerts2020.add(ChartData(month: "August", data: august1));
+    alerts2020.add(ChartData(month: "Sptember", data: september1));
+    alerts2020.add(ChartData(month: "October", data: october1));
+    alerts2020.add(ChartData(month: "November", data: november1));
+    alerts2020.add(ChartData(month: "December", data: december1));
+  }
+
+  Future<void> readYear2021() async {
+    final String response =
+        await rootBundle.loadString('assets/yearlyData.json');
+    final data = await json.decode(response) as List;
+    int jan = 0;
+    int feb = 0;
+    int march = 0;
+    int april = 0;
+    int may = 0;
+    int june = 0;
+    int july = 0;
+    int august = 0;
+    int september = 0;
+    int october = 0;
+    int november = 0;
+    int december = 0;
+    int jan1 = 0;
+    int feb1 = 0;
+    int march1 = 0;
+    int april1 = 0;
+    int may1 = 0;
+    int june1 = 0;
+    int july1 = 0;
+    int august1 = 0;
+    int september1 = 0;
+    int october1 = 0;
+    int november1 = 0;
+    int december1 = 0;
+
+    // ignore: avoid_function_literals_in_foreach_calls
+    data.forEach((element) {
+      var ts = element[0] * 86400000;
+      var date = DateTime.fromMillisecondsSinceEpoch(ts);
+      if (date.year == 2021) {
+        switch (date.month) {
+          case 1:
+            {
+              jan = jan + element[1] as int;
+              jan1 = jan1 + element[2] as int;
+            }
+
+            break;
+          case 2:
+            {
+              feb = feb + element[1] as int;
+              feb1 = feb1 + element[2] as int;
+            }
+
+            break;
+          case 3:
+            {
+              march = march + element[1] as int;
+              march1 = march1 + element[2] as int;
+            }
+            break;
+          case 4:
+            {
+              april = april + element[1] as int;
+              april1 = april1 + element[2] as int;
+            }
+            break;
+          case 5:
+            {
+              may = may + element[1] as int;
+              may1 = may1 + element[2] as int;
+            }
+            break;
+          case 6:
+            {
+              june = june + element[1] as int;
+              june = june1 + element[2] as int;
+            }
+            break;
+          case 7:
+            {
+              july = july + element[1] as int;
+              july = july1 + element[2] as int;
+            }
+            break;
+          case 8:
+            {
+              august = august + element[1] as int;
+              august1 = august1 + element[2] as int;
+            }
+            break;
+          case 9:
+            {
+              september = september + element[1] as int;
+              september1 = september1 + element[2] as int;
+            }
+            break;
+          case 10:
+            {
+              october = october + element[1] as int;
+              october1 = october1 + element[2] as int;
+            }
+            break;
+          case 11:
+            {
+              november = november + element[1] as int;
+              november1 = november1 + element[2] as int;
+            }
+            break;
+          case 12:
+            {
+              december = december + element[1] as int;
+              december1 = december1 + element[2] as int;
+            }
+            break;
+          default:
+        }
+      }
+    });
+    usage2021.add(ChartData(month: "janaury", data: jan));
+    usage2021.add(ChartData(month: "February", data: feb));
+    usage2021.add(ChartData(month: "March", data: march));
+    usage2021.add(ChartData(month: "April", data: april));
+    usage2021.add(ChartData(month: "May", data: may));
+    usage2021.add(ChartData(month: "June", data: june));
+    usage2021.add(ChartData(month: "July", data: july));
+    usage2021.add(ChartData(month: "August", data: august));
+    usage2021.add(ChartData(month: "Sptember", data: september));
+    usage2021.add(ChartData(month: "October", data: october));
+    usage2021.add(ChartData(month: "November", data: november));
+    usage2021.add(ChartData(month: "December", data: december));
+    alerts2021.add(ChartData(month: "janaury", data: jan1));
+    alerts2021.add(ChartData(month: "February", data: feb1));
+    alerts2021.add(ChartData(month: "March", data: march1));
+    alerts2021.add(ChartData(month: "April", data: april1));
+    alerts2021.add(ChartData(month: "May", data: may1));
+    alerts2021.add(ChartData(month: "June", data: june1));
+    alerts2021.add(ChartData(month: "July", data: july1));
+    alerts2021.add(ChartData(month: "August", data: august1));
+    alerts2021.add(ChartData(month: "Sptember", data: september1));
+    alerts2021.add(ChartData(month: "October", data: october1));
+    alerts2021.add(ChartData(month: "November", data: november1));
+    alerts2021.add(ChartData(month: "December", data: december1));
+  }
+
+  Future<void> readYear2022() async {
+    final String response =
+        await rootBundle.loadString('assets/yearlyData.json');
+    final data = await json.decode(response) as List;
+    int jan = 0;
+    int feb = 0;
+    int march = 0;
+    int april = 0;
+    int may = 0;
+    int june = 0;
+    int july = 0;
+    int august = 0;
+    int september = 0;
+    int october = 0;
+    int november = 0;
+    int december = 0;
+    int jan1 = 0;
+    int feb1 = 0;
+    int march1 = 0;
+    int april1 = 0;
+    int may1 = 0;
+    int june1 = 0;
+    int july1 = 0;
+    int august1 = 0;
+    int september1 = 0;
+    int october1 = 0;
+    int november1 = 0;
+    int december1 = 0;
+
+    // ignore: avoid_function_literals_in_foreach_calls
+    data.forEach((element) {
+      var ts = element[0] * 86400000;
+      var date = DateTime.fromMillisecondsSinceEpoch(ts);
+      if (date.year == 2022) {
+        switch (date.month) {
+          case 1:
+            {
+              jan = jan + element[1] as int;
+              jan1 = jan1 + element[2] as int;
+            }
+
+            break;
+          case 2:
+            {
+              feb = feb + element[1] as int;
+              feb1 = feb1 + element[2] as int;
+            }
+
+            break;
+          case 3:
+            {
+              march = march + element[1] as int;
+              march1 = march1 + element[2] as int;
+            }
+            break;
+          case 4:
+            {
+              april = april + element[1] as int;
+              april1 = april1 + element[2] as int;
+            }
+            break;
+          case 5:
+            {
+              may = may + element[1] as int;
+              may1 = may1 + element[2] as int;
+            }
+            break;
+          case 6:
+            {
+              june = june + element[1] as int;
+              june = june1 + element[2] as int;
+            }
+            break;
+          case 7:
+            {
+              july = july + element[1] as int;
+              july = july1 + element[2] as int;
+            }
+            break;
+          case 8:
+            {
+              august = august + element[1] as int;
+              august1 = august1 + element[2] as int;
+            }
+            break;
+          case 9:
+            {
+              september = september + element[1] as int;
+              september1 = september1 + element[2] as int;
+            }
+            break;
+          case 10:
+            {
+              october = october + element[1] as int;
+              october1 = october1 + element[2] as int;
+            }
+            break;
+          case 11:
+            {
+              november = november + element[1] as int;
+              november1 = november1 + element[2] as int;
+            }
+            break;
+          case 12:
+            {
+              december = december + element[1] as int;
+              december1 = december1 + element[2] as int;
+            }
+            break;
+          default:
+        }
+      }
+    });
+    usage2022.add(ChartData(month: "janaury", data: jan));
+    usage2022.add(ChartData(month: "February", data: feb));
+    usage2022.add(ChartData(month: "March", data: march));
+    usage2022.add(ChartData(month: "April", data: april));
+    usage2022.add(ChartData(month: "May", data: may));
+    usage2022.add(ChartData(month: "June", data: june));
+    usage2022.add(ChartData(month: "July", data: july));
+    usage2022.add(ChartData(month: "August", data: august));
+    usage2022.add(ChartData(month: "Sptember", data: september));
+    usage2022.add(ChartData(month: "October", data: october));
+    usage2022.add(ChartData(month: "November", data: november));
+    usage2022.add(ChartData(month: "December", data: december));
+    alerts2022.add(ChartData(month: "janaury", data: jan1));
+    alerts2022.add(ChartData(month: "February", data: feb1));
+    alerts2022.add(ChartData(month: "March", data: march1));
+    alerts2022.add(ChartData(month: "April", data: april1));
+    alerts2022.add(ChartData(month: "May", data: may1));
+    alerts2022.add(ChartData(month: "June", data: june1));
+    alerts2022.add(ChartData(month: "July", data: july1));
+    alerts2022.add(ChartData(month: "August", data: august1));
+    alerts2022.add(ChartData(month: "Sptember", data: september1));
+    alerts2022.add(ChartData(month: "October", data: october1));
+    alerts2022.add(ChartData(month: "November", data: november1));
+    alerts2022.add(ChartData(month: "December", data: december1));
+  }
+}
